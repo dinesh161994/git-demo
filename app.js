505d89221b6d6271032bd437a2789670d5fbfa48
dinesh-sirvi <dineshsirvi01@gmail.com>
2018-03-30 15:36:55 +0530

    resolved commit

--- app.js
+++ app.js
@@ -10,12 +10,9 @@ app.get('/:id', (req, res)=>{
 });
 console.log('Executes script');
 console.log('Some remote change');
-<<<<<<< HEAD
 console.log('parallel change from local');
-=======
 console.log('parallel change from remote');
 
->>>>>>> cfdc5881b5ac15e554e869ade71d10bca0b9c3f5
 app.listen(8090, ()=>{
     console.log('Server up on PORT 8090');
 });
