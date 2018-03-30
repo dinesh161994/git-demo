e6eedcc3534b3c4a88d3dbbe3051d27311357a52
dinesh-sirvi <dineshsirvi01@gmail.com>
2018-03-30 14:37:31 +0530

    Added get /id route

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/', (req,res)=>{
     res.status(200).json({msg: "Hello"});
 });
+app.get('/:id', (req, res)=>{
+    res.status(200).json({id: req.params.id});
+});
 
 app.listen(8090, ()=>{
     console.log('Server up on PORT 8090');
