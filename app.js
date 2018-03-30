ef96d478467c9d151ede87f1a22ba7b4ec80ac58
dinesh161994 <dchoudhary179@rku.ac.in>
2018-03-30 15:21:24 +0530

    Update app.js
    Remote changed app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.get('/:id', (req, res)=>{
     res.status(200).json({id: req.params.id});
 });
 console.log('Executes script');
+console.log('Some remote change');
 
 app.listen(8090, ()=>{
     console.log('Server up on PORT 8090');
